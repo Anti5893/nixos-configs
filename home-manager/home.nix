264da4da145093d89a564f6588afb555dd5f8ebf
Anti5893 <echo.savage.2004@gmail.com>
2024-03-10 00:58:56 +0000

    found where that curly brace went

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -51,7 +51,6 @@
     programs.zsh.enable = true;
       users.defaultUserShell = pkgs.zsh;
       programs.zsh.promptInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-    };
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
 
